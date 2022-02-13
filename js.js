bc08954aa161a846e72aa5efc524445ea2dcfa2e
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2022-02-13 19:49:54 +0330

    practice-javaScript1

---
+++ js.js
@@ -0,0 +1,179 @@
+// let total = 0, count = 1;
+// while(count <= 10){
+//     console.log(count);
+//     total += count;
+//     console.log(total);
+//     count += 1;
+// } 
+// console.log(total);
+// let x = 2 , y = `amir`;
+// console.log(typeof(x),typeof(y));
+// let y = "hi amir"
+// console.log("hello\"amir")
+// let x = `half 100 is ${100 / 2}`;
+// console.log(typeof(x));
+// let x = "";
+// let y = "!?";
+// console.log(x || y);
+// let xFalse = false;
+// console.log(xFalse ? 1 : 3);
+// let mood = "dark";
+// console.log(mood);
+// //light
+// mood = "light";
+// console.log(mood);
+// let luighsDebt = 140;
+// luighsDebt = luighsDebt - 35;
+// console.log(luighsDebt);
+// luighsDebt = luighsDebt - 30 ;
+// console.log(luighsDebt)
+// let tohi 
+// console.log(tohi)
+///////////////////////////////////
+
+// console.log(Math.max(2 ,3 , 5, 23, 2, 33, 13,23))
+
+// let num = Number(prompt('enter your num:'));
+// if(!Number(isNaN(num))) {
+//     console.log(`your number is the square root of ${num * num} `)
+// } else {
+//     console.log(`hey, why didn't you give me a number?`)
+// }
+
+// let number = Number(prompt('enter your code: '))
+// if (number < 10){
+//     console.log(`small`)
+// } else if (number < 100){
+//     console.log(`mediom`)
+// } else {
+//     console.log(`large`)
+// }
+// let num = 0;
+// while (num <= 12) {
+//     console.log(num);
+//     num = num + 2;
+// }
+// let num = 1;
+// let counter = 0;
+// while (counter < 10) {
+//     num = num * 2;
+//     counter =   counter + 1;
+// }
+// console.log(num)
+
+// let yourName;
+// do {
+//     yourName = prompt('who are you: ');
+
+// }while (!yourName);
+// console.log(yourName)
+
+// for(let x = 51; ; x = x + 1){
+//     if(x % 7 == 0 ){
+//         console.log(x)
+//         break;
+//     }
+// }                                                                
+
+// for (let num = 2; ; num *= 2){
+//     if(num % 4 == 0){
+//         console.log(num);
+//         break;
+//     }
+// }
+// switch(prompt("what is the whater like?")) {
+//     case "rany":
+//         console.log("remember to bring an umberlla.");
+//         break;
+//     case "sunny":
+//         console.log("go out.");
+//         break;
+//     case "cloudy":
+//         console.log("carefull");
+//         break;
+//     default:
+//         console.log("Unkown weather type!");
+//         break;
+
+// }
+
+// const square = function(x){
+//     return x * x;
+// }
+// console.log(square(12));
+// const makeNoise = function(){
+//     console.log("pling!");
+
+// };
+// makeNoise();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
